31625d8f1f2776d9ca21909072c881910a6f6711
anderleand <andersonleandro.desouza@gmail.com>
2021-06-23 21:40:06 -0300

    Fim do módulo 5

--- api-petshop/api/Serializador.js
+++ api-petshop/api/Serializador.js
@@ -1,19 +1,51 @@
+/* eslint-disable max-classes-per-file */
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado');
 
 class Serializador {
+  // eslint-disable-next-line class-methods-use-this
   json(dados) {
     return JSON.stringify(dados);
   }
 
   serializar(dados) {
     if (this.contentType === 'application/json') {
-      return this.json(dados);
+      return this.filtrar(dados);
     }
 
     throw new ValorNaoSuportado(this.contentType);
   }
+
+  filtrarObjeto(dados) {
+    const novoObjeto = {};
+    this.camposPublicos.forEach((campo) => {
+      if (dados.hasOwnProperty(campo)) {
+        novoObjeto[campo] = dados[campo];
+      }
+    });
+    console.log(JSON.stringify(novoObjeto));
+    return novoObjeto;
+  }
+
+  filtrar(dados) {
+    if (Array.isArray(dados)) {
+      dados = dados.map((item) => this.filtrarObjeto(item));
+    } else {
+      dados = this.filtrarObjeto(dados);
+    }
+    console.log(dados);
+    return dados;
+  }
+}
+
+class SerializadorFornecedor extends Serializador {
+  constructor(contentType) {
+    super();
+    this.contentType = contentType;
+    this.camposPublicos = ['id', 'empresa', 'categoria'];
+  }
 }
 module.exports = {
   Serializador,
+  SerializadorFornecedor,
   formatosAceitos: ['application/json'],
 };
--- api-petshop/api/routes/fornecedores/index.js
+++ api-petshop/api/routes/fornecedores/index.js
@@ -1,12 +1,16 @@
 const roteador = require('express').Router();
 const TabelaFornecedor = require('./TabelaFornecedor');
 const Fornecedor = require('./Fornecedor');
+const { SerializadorFornecedor } = require('../../Serializador');
 
 // Listar todos
 roteador.get('/', async (req, res) => {
   const result = await TabelaFornecedor.listar();
   res.status(200);
-  res.send(JSON.stringify(result));
+  const serializador = new SerializadorFornecedor(
+    res.getHeader('Content-Type'),
+  );
+  res.send(JSON.stringify(serializador.serializar(result)));
 });
 
 // Criar
@@ -16,6 +20,9 @@ roteador.post('/', async (req, res, next) => {
     const fornecedor = new Fornecedor(dadosRecebidos);
     await fornecedor.criar();
     res.status(201);
+    const serializador = new SerializadorFornecedor(
+      res.getHeader('Content-Type'),
+    );
     res.send(JSON.stringify(fornecedor));
   } catch (erro) {
     next(erro);
@@ -29,7 +36,10 @@ roteador.get('/:id', async (req, res, next) => {
     const fornecedor = new Fornecedor({ id });
     await fornecedor.carregar();
     res.status(200);
-    res.send(JSON.stringify(fornecedor));
+    const serializador = new SerializadorFornecedor(
+      res.getHeader('Content-Type'),
+    );
+    res.send(serializador.serializar(fornecedor));
   } catch (erro) {
     next(erro);
   }
@@ -44,7 +54,10 @@ roteador.put('/:id', async (req, res, next) => {
     const fornecedor = new Fornecedor(dados);
     await fornecedor.atualizar();
     res.status(200);
-    res.send(JSON.stringify({
+    const serializador = new SerializadorFornecedor(
+      res.getHeader('Content-Type'),
+    );
+    res.send(serializador.serializar({
       mensagem: 'Atualizado com sucesso.',
     }));
   } catch (erro) {
@@ -60,7 +73,10 @@ roteador.delete('/:id', async (req, res, next) => {
     await fornecedor.carregar();
     await fornecedor.delete();
     res.status(200);
-    res.send(JSON.stringify({
+    const serializador = new SerializadorFornecedor(
+      res.getHeader('Content-Type'),
+    );
+    res.send(serializador.serializar({
       mensagem: `Id ${id} removido com sucesso.`,
     }));
   } catch (erro) {
